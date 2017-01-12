8fff090b8b8a47ff9804026cc641f28eb1cdac78
bioinfornatics <bioinfornatics@fedoraproject.org>
2017-01-12 01:45:18 +0100

    style enhancement

--- runme.sh
+++ runme.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-TEXINPUTS="~/texmf:./texmf:$TEXINPUTS"
+export TEXINPUTS="~/texmf:./texmf:$TEXINPUTS"
 java -cp /opt/language_tool/3.5/languagetool-server.jar org.languagetool.server.HTTPServer --port 8081 &>/dev/null &
 xelatex main.tex && biber main && xelatex main.tex
