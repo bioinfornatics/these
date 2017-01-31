defbe961533f5a6ab844dd739ecbd6d25ed5f74d
bioinfornatics <bioinfornatics@fedoraproject.org>
2017-01-31 10:43:40 +0100

    count blank and black pages separately

--- pdf_color_pages_counter.sh
+++ pdf_color_pages_counter.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 declare -i pages=0
 declare -i pages_with_color=0
+declare -i pages_blank=0
+declare -i pages_black=0
 declare cyan=0
 declare magenta=0
 declare yellow=0
 declare black=0
+LANG=C
 
 equal_float(){
     result=false
-    if [[ $(bc <<< "$1 == $2") -eq 0 ]]; then
+    if [[ $(bc <<< "$1 == $2") -eq 1 ]]; then
         result=true
     fi
     echo "$result"
@@ -23,6 +26,12 @@ while read -r _cyan _magenta _yellow _black _; do
     ((pages++))
     
     if $(equal_float $_cyan 0) && $(equal_float $_magenta 0) && $(equal_float $_yellow 0); then
+        if $(equal_float $_black 0); then
+            ((pages_blank++))
+        else
+            ((pages_black++))
+        fi
+    else
         ((pages_with_color++))
     fi
     
@@ -32,4 +41,6 @@ done < <(gs -q -o - -sDEVICE=inkcov "$1")
 echo 'total pages:'"$pages"
 echo 'with color: '"$pages_with_color"
 echo 'without color: '$(bc <<< "$pages - $pages_with_color")
+echo 'black only: '"$pages_black"
+echo 'blank: '"$pages_blank"
 
